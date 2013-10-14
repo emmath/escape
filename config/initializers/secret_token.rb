44c5f5addba4268f766aae4b438f7ad90bdc0395
emmath <emmatrevena@gmail.com>
2013-10-14 11:02:46 -0700

    generate app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Escape::Application.config.secret_key_base = '59ef870f902f66bd65f883a7fee30121efca5cf90ec97468a34d7273afe158706056bfbda25801b119ef0433e340f38af4b9a5878e8efb54c988c9d214149d26'
